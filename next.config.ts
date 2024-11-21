ef9896cd108b111bd7eddd58785ac7693b708a96
astevens-ai <astevens81602@gmail.com>
2024-11-21 14:49:02 -0500

    fixed build errors

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [{
-      protocol: 'https',
-      hostname: 'example.com',
-      port: '',
-      pathname: '',
-    }]
-  },
+
 
 };
 
