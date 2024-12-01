7831f0ccaec3486058eafd453b96665c9455bfa6
astevens-ai <stevens_alexander@icloud.com>
2024-11-30 23:58:12 -0500

    update

--- src/app/profile/[userId]/page.jsx
+++ src/app/profile/[userId]/page.jsx
@@ -8,20 +8,17 @@ import Link from 'next/link'
 
 
 export default async function page({ params }) {
-    //db user id
-    const userID = (await params).userID
+    console.log("Params:", params); // Check if params are correct
 
-    //clerk user id
-    const { userId } = await auth()
-    console.log("Clerk userId:", userId); // This should not be null
-
+    const { userID } = params;
+    const { userId } = await auth();
+    console.log("UserID:", userID);
+    console.log("Clerk userId:", userId);
 
     const [user, posts] = await Promise.all([
         fetchUser({ userId: userID }),
         fetchUserPosts({ userId: userID }),
-    ])
-    console.log(posts)
-
+    ]);
 
     return (
         <div className='bg-[#200c22] text-white pt-12'>
@@ -32,10 +29,10 @@ export default async function page({ params }) {
                 <div className='flex flex-col justify-center gap-4 align-middle items-center h-full min-h-72 text-lg'>
                     <p className=''>No posts yet!</p>
                     <Link className='p-2 bg-purple-500 rounded-lg' href={'/create-post'}>Create Post</Link>
-                </div>) : (
+                </div>
+            ) : (
                 <ProfileContent posts={posts} />
             )}
-
         </div>
-    )
+    );
 }
