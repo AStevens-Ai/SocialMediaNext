550442b04b6610b3abe85dc1630a55a0af7bb3f5
astevens-ai <stevens_alexander@icloud.com>
2024-11-30 22:23:39 -0500

    changing middleware file to test if it fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// Exclude /sign-up and /sign-in from protection
+const isPublicRoute = createRouteMatcher(['/sign-up(.*)', '/sign-in(.*)'])
 export default clerkMiddleware(async (auth, req) => {
-    console.log("Incoming Request:", req.url);
-    const session = await auth.protect();
-
-    if (!session) {
-        console.error("No session found!");
-    } else {
-        console.log("Authenticated Clerk User:", session.userId);
+    // Protect all routes except the public routes
+    if (!isPublicRoute(req)) {
+        await auth.protect();
     }
 });
 
+// Configure the middleware to apply to relevant routes
 export const config = {
     matcher: [
         '/',                      // Home route
+        '/profile/(.*)',         // Match dynamic routes like /profile/[userID]
         '/sign-up(.*)',            // Match /sign-up and any sub-routes under it
         '/sign-in(.*)',            // Match /sign-in and any sub-routes under it
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
